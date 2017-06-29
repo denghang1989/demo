80b680cd348fcf7513f14b15341ac072e8889b8f
denghang <hang.deng@fgecctv.com>
2017-06-29 11:39:13 +0800

    编写mqttclient的代码

---
+++ databinding_demo/src/main/java/com/example/databinding_demo/simple_image_hold_error/HolderImageActivity.java
@@ -0,0 +1,17 @@
+package com.example.databinding_demo.simple_image_hold_error;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @date 2017/6/22 11
+ */
+public class HolderImageActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
